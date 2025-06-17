1ea1e7e8a3dd46f98325180d8f4ff2f7f425ec45
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-06-18 06:17:42 +0700

    fix

--- dev-games/bevy/files/main.rs
+++ dev-games/bevy/files/main.rs
@@ -1,13 +1,5 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_system(hello_world)
-        .run();
-}
-
-fn hello_world() {
     println!("Hello Bevy!");
 }
-
